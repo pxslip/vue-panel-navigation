b2cc653341ff4daa26aae447202adf222e5062dc
Will K (pxslip) <dev@pxslip.com>
2022-06-23 15:24:49 +0000

    Code style cleanup

--- src/components/mixins/NavMixin.js
+++ src/components/mixins/NavMixin.js
@@ -1,60 +1,57 @@
-import Vue from 'vue';
 export default {
-    props: {
-        classes: {
-            type: Object,
-            default () {
-                return {
-                    textSectionUl: ['vp--text-sections'],
-                    textSectionLi: ['vp--text', 'vp--section'],
-                    textPanelUl: ['vp--text-panels'],
-                    textPanelLi: ['vp--text', 'vp--panel'],
-                    dotSectionUl: ['vp--dot-sections'],
-                    dotSectionLi: ['vp--dot', 'vp--section'],
-                    dotPanelUl: ['vp--dot-panels'],
-                    dotPanelLi: ['vp--dot', 'vp--panel']
-                }
-            }
-        }
-    },
-    data() {
+  props: {
+    classes: {
+      type: Object,
+      default() {
         return {
-            sections: [],
-            panels: {}
-        }
+          textSectionUl: ["vp--text-sections"],
+          textSectionLi: ["vp--text", "vp--section"],
+          textPanelUl: ["vp--text-panels"],
+          textPanelLi: ["vp--text", "vp--panel"],
+          dotSectionUl: ["vp--dot-sections"],
+          dotSectionLi: ["vp--dot", "vp--section"],
+          dotPanelUl: ["vp--dot-panels"],
+          dotPanelLi: ["vp--dot", "vp--panel"],
+        };
+      },
     },
-    methods: {
-        scrollTo(elem) {
-            this.$scrollTo(elem);
-        },
-        hasClass(elem, classToAdd) {
-            return elem.classList.contains(classToAdd);
-        },
-        sectionClicked(section, $event) {
-            if (!this.$listeners.sectionclicked) {
-                // handle the click ourselves
-                this.scrollTo(section);
-            } else {
-                let vm = this;
-                this.$emit('sectionclicked', section, $event, function () {
-                    vm.scrollTo(section);
-                });
-            }
-        },
-        panelClicked(panel, $event) {
-            if (!this.$listeners.panelclicked) {
-                // handle the click ourselves
-                this.scrollTo(panel);
-            } else {
-                let vm = this;
-                this.$emit('panelclicked', panel, $event, function () {
-                    vm.scrollTo(panel);
-                });
-            }
-        }
+  },
+  data() {
+    return {
+      sections: [],
+      panels: {},
+    };
+  },
+  methods: {
+    scrollTo(elem) {
+      this.$scrollTo(elem);
     },
-    created() {
-        this.sections = this.$getSections();
-        this.panels = this.$getPanels();
-    }
-}
+    hasClass(elem, classToAdd) {
+      return elem.classList.contains(classToAdd);
+    },
+    sectionClicked(section, $event) {
+      if (!this.$listeners.sectionclicked) {
+        // handle the click ourselves
+        this.scrollTo(section);
+      } else {
+        this.$emit("sectionclicked", section, $event, () => {
+          this.scrollTo(section);
+        });
+      }
+    },
+    panelClicked(panel, $event) {
+      if (!this.$listeners.panelclicked) {
+        // handle the click ourselves
+        this.scrollTo(panel);
+      } else {
+        this.$emit("panelclicked", panel, $event, () => {
+          this.scrollTo(panel);
+        });
+      }
+    },
+  },
+  created() {
+    this.sections = this.$getSections();
+    this.panels = this.$getPanels();
+  },
+};
